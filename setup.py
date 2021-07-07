4df88b7826e45a241cda43181c60a10158f07c18
godzilla-but-nicer <patgwall@iu.edu>
2021-07-07 11:57:29 -0400

    better function for this

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='casim',
-    version='0.4.4',
+    version='0.4.5',
     description='Simple package for simulating cellular automata',
     url='https://github.com/godzilla-but-nicer/cellularautomata',
     author='Pat Wall',
